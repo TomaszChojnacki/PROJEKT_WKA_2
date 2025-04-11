8f4e82560690aba310e7537daf27d184edeac9ee
TomaszChojnacki <tomaszchojnacki3@gmail.com>
2025-04-11 20:16:06 +0200

    Dodanie zbijajacego sie szkla w aucie

---
+++ Source/Projekt_PGK_WKAEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Projekt_PGK_WKAEditorTarget : TargetRules
+{
+	public Projekt_PGK_WKAEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+
+		ExtraModuleNames.AddRange( new string[] { "Projekt_PGK_WKA" } );
+	}
+}
